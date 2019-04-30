a73ee356b920fe4f948b22891d405cc0f668313d
Jorge Miranda <jorgex66@hotmail.com>
2019-04-29 22:29:22 -0500

    Lista con JS

--- interact.js
+++ interact.js
@@ -1,1 +1,13 @@
-angular.module('pruebaApp', [])
+angular.module('list', [])
+	.controller('listcontroller', function function_name($scope) {
+
+	$scope.entry = " ";
+	
+	$scope.content = [];
+	
+				$scope.save = function(){
+				$scope.content.push($scope.entry);
+				$scope.entry = "";
+				}
+	})
+;
